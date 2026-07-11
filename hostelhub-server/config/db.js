1f52fa9deb65d94911aa15ae8ba252efe86f21cc
Kanha-dalai <kanhadalai2098@gmail.com>
2026-07-11 11:32:41 +0530

    Remove MongoDB URL from server logs

--- hostelhub-server/config/db.js
+++ hostelhub-server/config/db.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        console.log("Mongo URL:", process.env.MONGO_URL);
-
         await mongoose.connect(process.env.MONGO_URL);
 
         console.log("✅ MongoDB Connected Successfully");
