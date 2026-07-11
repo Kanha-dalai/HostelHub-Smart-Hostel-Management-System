4e6ceb61c66001bb386b45565f080b387eec37f5
Kanha-dalai <kanhadalai2098@gmail.com>
2026-07-11 11:08:59 +0530

    Fix MongoDB connection and prepare backend for deployment

--- hostelhub-server/config/db.js
+++ hostelhub-server/config/db.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
-const connectDB = async() => {
-    try{
+const connectDB = async () => {
+    try {
+        console.log("Mongo URL:", process.env.MONGO_URL);
+
         await mongoose.connect(process.env.MONGO_URL);
-        console.log("MongoDB Connected Successfully");
-    }
-    catch(error){
-        console.error("MongoDB Connection Failed");
-        console.error(error.message);
+
+        console.log("✅ MongoDB Connected Successfully");
+    } catch (error) {
+        console.error("Full Error:");
+        console.error(error);
         process.exit(1);
     }
 };
